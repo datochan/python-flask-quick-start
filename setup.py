3029faf856501af223d0cf59afb8f04fe87bfe6c
datochan <datochan@qq.com>
2021-09-07 16:46:26 +0800

    提交初始代码

--- setup.py
+++ setup.py
@@ -5,14 +5,23 @@
 
     :copyright: (c) 2019-10-08 by datochan.
 """
-from setuptools import setup
-
 from setuptools import setup, find_packages
 
 setup(
-    name='python-flask-quick-start',
-    version='1.0',
+    author="datochan",
+    author_email="datochan@qq.com",
     description="An python3 flask web framework template.",
+    install_requires=[
+        "Flask==1.1.1",
+        "Flask-HTTPAuth==3.3.0",
+        "Flask-Login==0.4.1",
+        "Flask-Migrate==2.5.2",
+        "Flask-Principal==0.4.0",
+        "Flask-Script==2.0.6",
+        "Flask-Security==3.0.0",
+    ],
+    license="MIT",
+    name='python-flask-quick-start',
     packages=find_packages(),
-    entry_points={'core': ['settings = dirbot.settings']},
+    version='1.0',
 )
