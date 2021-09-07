866e85e6d2f411de1fc95b6689c4c22ca9f3bff4
datochan <datochan@qq.com>
2021-09-07 18:53:43 +0800

    调整项目结构

--- quick_flask/views.py
+++ quick_flask/views.py
@@ -5,9 +5,9 @@
     项目的演示代码.
     :copyright: (c) 2019-10-08 by datochan.
 """
-from quick_flask.app import __app__
+from quick_flask import quick_flask
 
 
-@__app__.route('/', methods=['GET', 'POST'])
+@quick_flask.route('/', methods=['GET', 'POST'])
 def index():
     return 'Hello, It Works!'
